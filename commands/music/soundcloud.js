2ed81e3de6f1c364e660763a2a1dc2231fc8b69c
Nikola6825 <nikolathegamer547398307039@gmail.com>
2022-09-24 18:50:25 +0200

    addedfiles1

---
+++ commands/music/soundcloud.js
@@ -0,0 +1,7 @@
+module.exports ={ 
+  /*YOU NEED TO ADD SOUNDCLOUD CLIENT ID FOR THIS TO WORK*/
+  name:"soundcloud",
+  aliases:["sc"],
+  code:`
+  $playTrack[soundcloud;message]`
+}
---
+++ commands/music/youtube.js
@@ -0,0 +1,6 @@
+module.exports ={
+  name:"youtube",
+  aliases:["yt","play"],
+  code:`
+  $playTrack[youtube;message]`
+}
